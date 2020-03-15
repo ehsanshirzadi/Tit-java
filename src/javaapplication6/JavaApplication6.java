002465725b1d48df15855ca4afbe501b1da81146
ehsanshirzadi <ehsan.shirzadi74@gmail.com>
2020-03-15 04:06:46 +0330

    Start

--- src/javaapplication6/JavaApplication6.java
+++ src/javaapplication6/JavaApplication6.java
@@ -1,121 +1,99 @@
-
-
 package javaapplication6;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.regex.*;  
+import java.util.regex.*;
 import javax.print.DocFlavor;
+
 public class JavaApplication6 {
 
-    
-  
-    static String readFile(String address)
-    {
+    static String readFile(String address) {
         try {
-              File myObj = new File(address);
-              Scanner myReader = new Scanner(myObj);
-              String lines="";      
-              while (myReader.hasNextLine()) {
+            File myObj = new File(address);
+            Scanner myReader = new Scanner(myObj);
+            String lines = "";
+            while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                
-                lines=lines+"\n\r"+data;        
-              }
-              myReader.close();
-              return lines.toLowerCase();
-            } catch (FileNotFoundException e) {
-               System.out.println("An error occurred."+" "+"\n\r"+e);
-              return null;
-            }
-    }
-    static boolean matchText(String reg,String input)
-    {
-        try
-          {
-            Pattern p = Pattern.compile(reg);
-            Matcher m = p.matcher(input);    
-             if (m.find()==true){
-                return true;
-             }
-             else
-             {
-                return false;
-             }
-          }
-    catch(Exception e)
-          {
-             System.out.println("An error occurred."+" "+"\n\r"+e);
-             return false;
-          }
-    }
-    static void find(String directory,String reg)
-    {
-        try
-        {
-              File dir=new File(directory);
-              for(File name:dir.listFiles())
-              {
 
-                  System.out.println(name);
-                  System.out.println( matchText(reg, readFile(name.getAbsolutePath())));
-              }
-        
-        }catch(Exception e)
-        {
-              System.out.println("An error occurred."+" "+"\n\r"+e);
+                lines = lines + "\n\r" + data;
+            }
+            myReader.close();
+            return lines.toLowerCase();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred." + " " + "\n\r" + e);
+            return null;
         }
     }
-    static String makeregx(String searchText)
-    {
-        try
-        {
-         String reg = "";
-                String[] sp = searchText.toLowerCase().split(" ");
-                for (String item : sp)
-                {
-                    if (item.equals("or"))
-                    {
+
+    static boolean matchText(String reg, String input) {
+        try {
+            Pattern p = Pattern.compile(reg);
+            Matcher m = p.matcher(input);
+            return m.find();
+            
+        } catch (Exception e) {
+            System.out.println("An error occurred." + " " + "\n\r" + e);
+            return false;
+        }
+    }
+
+    static void find(String directory, String reg) {
+        try {
+            File dir = new File(directory);
+            for (File name : dir.listFiles()) {
+
+                System.out.println(name);
+                System.out.println(matchText(reg, readFile(name.getAbsolutePath())));
+            }
+
+        } catch (Exception e) {
+            System.out.println("An error occurred." + " " + "\n\r" + e);
+        }
+    }
+
+    static String makeregx(String searchText) {
+        try {
+            String reg = "";
+            String[] sp = searchText.toLowerCase().split(" ");
+            for (String item : sp) {
+                switch (item) {
+                    case "or":
                         reg = reg + "|";
-                    }
-                    else if (item.equals("and") )
-                    {
+                        break;
+                    case "and":
                         reg = reg + "\\s*";
-                    }
-                    else if (item.equals("")) continue;
-                    else
-                    {
+                        break;
+                    case "":
+                        continue;
+                    default:
                         reg = reg + "(" + item + ")";
-                    }
+                        break;
                 }
-                System.out.println(reg);
-                return reg;
-        }
-        catch(Exception e)
-        {
-             System.out.println("An error occurred."+" "+"\n\r"+e);
-             return null;
+            }
+          
+            return reg;
+        } catch (Exception e) {
+            System.out.println("An error occurred." + " " + "\n\r" + e);
+            return null;
         }
     }
-    
+
     public static void main(String[] args) {
-      try
-      {
-         Scanner in = new Scanner(System.in); 
-         System.out.println("\\\\Please enter directory address : ");
-         String address,searchText;
-         address=in.nextLine();
-          System.out.println("\\\\Please enter your keywords : ");
-         searchText=in.nextLine();         
-         find(address,makeregx(searchText));
-      }
-      catch(Exception e)
-      {
-       System.out.println("An error occurred."+" "+"\n\r"+e);
-      }
-         
-         
-     
+        try {
+             System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\"+"Welcome"+"////////////////"+"\n\r");
+            Scanner in = new Scanner(System.in);
+            System.out.println("\\\\Please enter directory address : ");
+            String address, searchText;
+            address = in.nextLine();
+            System.out.println("\\\\Please enter your keywords : ");
+            searchText = in.nextLine();
+            find(address, makeregx(searchText));
+        } catch (Exception e) {
+            System.out.println("An error occurred." + " " + "\n\r" + e);
+        }
+
     }
-    
+
 }
